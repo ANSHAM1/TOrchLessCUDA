88f3f8273e9599cc7eadf6d16824661449c2067d
ANSHAM MAURYA <anshammaurya2291@gmail.com>
2026-07-23 21:51:53 +0530

    cuda path hardcoded fix

--- include/module.hpp
+++ include/module.hpp
@@ -1,22 +1,22 @@
-#pragma once
-
-#include "tensor.hpp"
-
-
-
-class Layer {
-protected:
-	Tensor InputTensor;
-	std::vector<size_t> OutputShape;
-
-public:
-	virtual void forward(const Tensor<T>& Input, Tensor<T>& Output) = 0;
-
-	//virtual void backward(const Tensor<T>& UpstreamGrad, Tensor<T>& DownstreamGrad) = 0;
-
-	virtual const std::vector<size_t>& getOutputShape() const {
-		return OutputShape;
-	}
-
-	virtual ~Layer() {}
-};
+//#pragma once
+//
+//#include "tensor.hpp"
+//
+//
+//
+//class Layer {
+//protected:
+//	Tensor InputTensor;
+//	std::vector<size_t> OutputShape;
+//
+//public:
+//	virtual void forward(const Tensor<T>& Input, Tensor<T>& Output) = 0;
+//
+//	//virtual void backward(const Tensor<T>& UpstreamGrad, Tensor<T>& DownstreamGrad) = 0;
+//
+//	virtual const std::vector<size_t>& getOutputShape() const {
+//		return OutputShape;
+//	}
+//
+//	virtual ~Layer() {}
+//};
